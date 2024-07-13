5a07983d7ff67db2860c978bc2777089bc2c24ab
Lars-Christian Schulz <lschulz@ovgu.de>
2024-07-13 18:13:11 +0200

    Adjust SCION layer bindings
    Remove ports commonly used for SCION end host communication from UDP
    port range identified as SCION.

--- scapy_scion.py
+++ scapy_scion.py
@@ -6,10 +6,9 @@ from scapy_scion.layers.scmp import *
 from scapy_scion.layers.bfd import *
 from scapy_scion.layers.idint import *
 
-
 # Additional layers bindings
 # See https://github.com/scionproto/scion/blob/master/tools/wireshark/scion.lua
-for port in range(30000, 32000):
+for port in range(31000, 32000):
     bind_bottom_up(UDP, SCION, dport=port)
     bind_bottom_up(UDP, SCION, sport=port)
 for port in range(40000, 40050):
@@ -18,7 +17,6 @@ for port in range(40000, 40050):
 for port in range(50000, 50050):
     bind_bottom_up(UDP, SCION, dport=port)
     bind_bottom_up(UDP, SCION, sport=port)
-
 
 if __name__ == "__main__":
     interact(mydict=globals())
